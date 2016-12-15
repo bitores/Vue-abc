7fafb35a0ff1d61c84ac1566de117463f7fca118
huangzhengjie <huangzhengjie@kuaiqiangche.com>
2016-12-15 11:09:13 +0800

    添加新组件,及各种组件注册方式案例

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Route Alias</h1>
+    <h1 @click="getParams">Route Alias</h1>
     <ul>
       <li>
         <router-link to="/home/foo/1/44">Foo</router-link>
@@ -12,13 +12,51 @@
         <router-link to="/home/baz">Baz</router-link>
       </li>
     </ul>
+    <h4>{{classObject}}</h4>
+    <child></child>
+    <!-- <mychild></mychild> -->
+    <inner></inner>
+    <child2></child2>
+    <child3></child3>
+    <child4></child4>
+    <child5></child5>
+    <!-- <child6></child6>
+    <myChild></myChild> -->
     <router-view>
     </router-view>
   
   </div>
 </template>
 
+<script type="text/x-template" id="child-template"></script>
+
 <script>
+import Vue from 'vue';
+import Inner from './components/Inner.vue';
+
+var Child = { // 外定义， 内注册
+  template: '<div>A custom component! —— child</div>'
+} 
+
+var mychild = new Vue({ // 这样定义是！！！！错误的！！，new Vue只能通过 el来绑定元素
+  template: '<div>A custom component!</div>'
+  // el:"#child-template"
+})
+
+// Vue.component('child6', myChild)
+
+var myChild = Vue.extend({
+  template: '<div>A custom component!___child4/5</div>'
+})
+
+var child5 = Vue.component('child4', myChild)
+
+
+Vue.component('child2', { // 外定义，外注册
+  template: '<div>A custom component! - -child2</div>'
+})
+
+
 export default {
   name: 'app',
   data () {
@@ -35,10 +73,27 @@ export default {
     '$route':'getParams'
   },
 
+  computed: {
+    classObject: function() {
+      return 'classObject'
+    }
+  },
+
   methods: {
      getParams() {
         console.log(this.$route.params)
      }
+  },
+
+  components: {
+    Child,
+    Inner,
+    "child3": { // 内定义内注册
+      template: '<div>A custom component! ^^ child3</div>'
+    },
+    child5,
+    // child6,
+    // myChild
   }
 }
 </script>
---
+++ src/components/Inner.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="inner">
+		<h1>{{named}}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		// data: {
+		// 	named: "Inner"
+		// }
+
+
+		data() {
+			return {
+				named: "Inner"
+			}
+		}
+	}
+</script>
+
+<style>
+	#inner {
+		background: white;
+		color: red;
+	}
+</style>
